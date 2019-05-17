84f97ae146fd39708e41ceaa8b6717c83d5cb7c7
Savchuk <iv.savchuk@gmail.com>
2019-05-17 12:36:57 +0300

    test

--- cron_import_1.php
+++ cron_import_1.php
@@ -3,9 +3,10 @@
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
+
 require_once('lib/import/Import.php');
 
-$import_ajax = new ImportAjax();
+$import = new ImportCron();
 
 $import->send("Cron");
 
